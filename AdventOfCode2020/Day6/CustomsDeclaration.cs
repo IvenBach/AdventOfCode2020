debfd15865fd3df4ce14a200253bda2152b1c559
IvenBach <ivenbach@gmail.com>
2020-12-10 15:45:42 -0800

    Day6 Part1

---
+++ AdventOfCode2020/Day6/CustomsDeclaration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Day6
+{
+    public class CustomsDeclaration
+    {
+        public int TrueAnswerCount { get; }
+        public CustomsDeclaration(string input)
+        {
+             TrueAnswerCount = input.Replace(Environment.NewLine, string.Empty)
+                .Distinct()
+                .Count();
+        }
+
+        public static string[] ConvertRawInput(string input)
+        {
+            return input.Split(new[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static int GroupCount(IEnumerable<string> groups)
+        {
+            int count = 0;
+            foreach (var group in groups)
+            {
+                var cd = new CustomsDeclaration(group);
+                count += cd.TrueAnswerCount;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ TestsForAdventOfCode2020/Day6/TestCustomsDeclaration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsForAdventOfCode2020.Day6
+{
+    [TestClass]
+    public class TestCustomsDeclaration
+    {
+        [TestMethod]
+        public void Example_input_produces_exected_result()
+        {
+            const string input = @"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+            var groups = AdventOfCode2020.Day6.CustomsDeclaration.ConvertRawInput(input);
+            var actual = AdventOfCode2020.Day6.CustomsDeclaration.GroupCount(groups);
+
+            Assert.AreEqual(11, actual);
+        }
+
+        [TestMethod]
+        [DataRow("abc", 3)]
+        [DataRow("aaaaa", 1)]
+        public void Count_matches_distinct_values(string input, int expected)
+        {
+            var cd = new AdventOfCode2020.Day6.CustomsDeclaration(input);
+
+            var actual = cd.TrueAnswerCount;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
