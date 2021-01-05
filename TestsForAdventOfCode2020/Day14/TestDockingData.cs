fc9b774343a10e39e7581142160bb7f4de2a6002
IvenBach <ivenbach@gmail.com>
2021-01-05 14:46:28 -0800

    Correct test names

--- TestsForAdventOfCode2020/Day14/TestDockingData.cs
+++ TestsForAdventOfCode2020/Day14/TestDockingData.cs
@@ -14,7 +14,7 @@ namespace TestsForAdventOfCode2020.Day14
         [DataRow(0b1011, 0b1001001)]
         [DataRow(0b1100101, 0b1100101)]
         [DataRow(0b0, 0b1000000)]
-        public void Sample_mask_converts_given_number_to_expected(int given, int expected)
+        public void Part1_sample_mask_converts_given_number_to_expected(int given, int expected)
         {
             var input = new[]
             {
@@ -29,7 +29,7 @@ namespace TestsForAdventOfCode2020.Day14
         }
 
         [TestMethod]
-        public void Sample_inputs_match_expected_output()
+        public void Part1_sample_inputs_match_expected_output()
         {
             var dd = new AdventOfCode2020.Day14.DockingData(AdventOfCode2020.Inputs.Day14Sample());
 
